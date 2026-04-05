b74a022c7fd25fa33d5a006f5ba1666f7af2813f
Guillaume G <95032512+L1nkiZ@users.noreply.github.com>
2026-04-05 22:08:03 +0200

    Modification pour le lint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,31 +1,28 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true, jest: true, node: true },
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
+  root: true,
+  env: { browser: true, es2020: true, jest: true, node: true },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['@typescript-eslint', 'react-refresh', 'react-hooks'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-    settings: { react: { version: 'detect' } },
-    plugins: ['@typescript-eslint', 'react-refresh', 'react-hooks'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            {
-                argsIgnorePattern: '^_',
-                varsIgnorePattern: '^_',
-            },
-        ],
-    },
-};
+  },
+}
--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,27 +1,27 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    moduleNameMapper: {
-        '\\.(css|scss)$': '<rootDir>/test/styleMock.cjs',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: {
-                    jsx: 'react-jsx',
-                },
-            },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/test/styleMock.cjs',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+        },
+      },
+    ],
+    '^.+\\.jsx?$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          ['@babel/preset-react', { runtime: 'automatic' }],
         ],
-        '^.+\\.jsx?$': [
-            'babel-jest',
-            {
-                presets: [
-                    ['@babel/preset-env', { targets: { node: 'current' } }],
-                    ['@babel/preset-react', { runtime: 'automatic' }],
-                ],
-            },
-        ],
-    },
-};
+      },
+    ],
+  },
+}
