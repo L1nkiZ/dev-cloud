371657ff5adbc5d4a845ef05a556637fefe0d340
Guillaume G <guillaume.gerard2112@outlook.fr>
2026-06-15 11:11:56 +0200

    feat : Ajout de prometheus et Grafana pour géré les logs de l'application

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
     testEnvironment: 'node',
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
     transform: {
         '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
         '^.+\\.js$': [
